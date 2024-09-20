220cf3336c7282d5c90193b9a95a22e15194269a
Gonzalo Kerbs <gonzakerbs@gmail.com>
2024-09-20 20:01:55 -0300

    fix waves buf

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -46,68 +46,9 @@ export default function Layout() {
             </div>
            </div>
         </div>
-        <footer className='absolute right-0 left-0 bottom-0 w-[1600px]'>
+        <footer className='absolute right-0 left-0 bottom-0 w-full'>
           <div className="background">
-            <svg
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              width="100%"
-              height="100%"
-              viewBox="0 0 1600 900"
-            >
-              <defs>
-                <path
-                  id="wave"
-                  fill="#0ea5e9"
-                  d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-          s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
-                />
-              </defs>
-              <g>
-                <use xlinkHref="#wave" opacity=".3">
-                  <animateTransform
-                    attributeName="transform"
-                    attributeType="XML"
-                    type="translate"
-                    dur="8s"
-                    calcMode="spline"
-                    values="270 230; -334 180; 270 230"
-                    keyTimes="0; .5; 1"
-                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                    repeatCount="indefinite"
-                  />
-                </use>
-                <use xlinkHref="#wave" opacity=".5">
-                  <animateTransform
-                    attributeName="transform"
-                    attributeType="XML"
-                    type="translate"
-                    dur="6s"
-                    calcMode="spline"
-                    values="-270 230;243 220;-270 230"
-                    keyTimes="0; .6; 1"
-                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                    repeatCount="indefinite"
-                  />
-                </use>
-                <use xlinkHref="#wave" opacity=".7">
-                  <animateTransform
-                    attributeName="transform"
-                    attributeType="XML"
-                    type="translate"
-                    dur="4s"
-                    calcMode="spline"
-                    values="0 230;-140 200;0 230"
-                    keyTimes="0; .4; 1"
-                    keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-                    repeatCount="indefinite"
-                  />
-                </use>
-              </g>
-            </svg>
+            <img src="/wave.svg" alt="animated waves" className="w-full h-full" />
           </div>
         </footer>
       </div>

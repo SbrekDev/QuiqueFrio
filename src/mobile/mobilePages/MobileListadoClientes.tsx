15dda1d58a69a1c4a144e23b61d3a0d9d2bf4cca
Gonzalo Kerbs <gonzakerbs@gmail.com>
2024-10-15 14:38:34 -0300

    add search by date

--- src/mobile/mobilePages/MobileListadoClientes.tsx
+++ src/mobile/mobilePages/MobileListadoClientes.tsx
@@ -9,6 +9,7 @@ export default function MobileListadoClientes() {
 
   
     const [ estadoFinal, setEstadoFinal] = useState<string>('Todos')
+    const [ finalDate, setFinalDate] = useState<string>('')
     const [ searchTerms, setSearchTerms] = useState<string>('')
     const { clientes, getClientes, isLoading } = useClientStore();
 
@@ -20,6 +21,10 @@ export default function MobileListadoClientes() {
 
       const handleChange = (e: string) => {
         setEstadoFinal(e)   
+      }
+
+      const handleDateChange = (e: string) => {
+        setFinalDate(e)  
       }
 
       const handleSearchChange = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -36,9 +41,19 @@ export default function MobileListadoClientes() {
 
     <div className="flex flex-col items-center mx-auto p-2 mt-24 h-screen w-full">
           <div className="bg-white shadow-md rounded-md p-5 pl-10 pr-10 w-full mb-5 flex flex-col items-start">
-            <h2 className="text-3xl mb-3 text-slate-600 w-full flex justify-center items-center">Filtros</h2>
+            <h2 className="text-4xl mb-3 text-slate-600 w-full flex justify-center items-center font-bold">Filtros</h2>
               <div className="flex gap-3 items-center justify-between w-full">
-                <p className="text-slate-400 font-bold w-40">Estado Actual</p>
+                <p className="text-slate-400 font-bold w-32">Nombre</p>
+                <input 
+                  type="text"
+                  id="buscar"
+                  placeholder="Nombre y Apellido"
+                  className="p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50 w-full" 
+                  onChange={handleSearchChange}
+                />
+              </div>
+              <div className="flex gap-3 items-center justify-between w-full">
+                <p className="text-slate-400 font-bold w-32">Estado</p>
                 <select
                   id="estadoSelectFind"
                   className="w-full bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50 p-2"
@@ -48,17 +63,15 @@ export default function MobileListadoClientes() {
                     <option value="Completado">Completados</option>
                 </select>
               </div>
-              <div className="flex gap-3 items-center justify-between w-full">
-                <p className="text-slate-400 font-bold">Nombre</p>
-                <input 
-                  type="text"
-                  id="buscar"
-                  placeholder="Nombre y Apellido"
-                  className="p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50 w-full" 
-                  onChange={handleSearchChange}
-                />
+              <div className="flex gap-3 items-center justify-between w-full ">
+                  <p className="text-slate-400 font-bold w-32">Fecha</p>
+                  <input 
+                    type="date"
+                    id="date"
+                    className="w-full p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50" 
+                    onChange={(e) => handleDateChange(e.target.value)}
+                  />
               </div>
-
 
 
                 
@@ -67,7 +80,7 @@ export default function MobileListadoClientes() {
          <div className={`flex flex-col w-full space-y-3 h-screen`}>
          {clientesFiltrados.length ? (
                 <>
-                  {estadoFinal === 'Todos' ? 
+                  {estadoFinal === 'Todos' && finalDate.toString() === '' ? 
                     clientesFiltrados.map(cliente => 
                       (
                         <MobileClientDetails 
@@ -78,15 +91,23 @@ export default function MobileListadoClientes() {
                     )    
                   :                   
                   clientesFiltrados.map(cliente => 
-                    cliente.estado === estadoFinal ? (
+                    cliente.estado === estadoFinal && finalDate === '' ? (
                       <MobileClientDetails 
                         key={cliente.id}
                         cliente={cliente}
                       />
-                    ) : null
+                    ) : cliente.estado === estadoFinal && cliente.ultimaVisita.toString() === finalDate ? (
+                      <MobileClientDetails 
+                        key={cliente.id}
+                        cliente={cliente}
+                      />
+                    ) : cliente.ultimaVisita.toString() === finalDate && estadoFinal === 'Todos' ? (
+                      <MobileClientDetails 
+                        key={cliente.id}
+                        cliente={cliente}
+                      />  ) : null
                   )                  
                   }
-                  <div className="bg-slate-400 w-full min-h-4 rounded-full"></div>
                 </>
               ) : (
                 <>
--- src/pages/ListadoClientes.tsx
+++ src/pages/ListadoClientes.tsx
@@ -9,6 +9,7 @@ export default function ListadoClientes() {
 
   
     const [ estadoFinal, setEstadoFinal] = useState<string>('Todos')
+    const [ finalDate, setFinalDate] = useState<string>('')
     const [ searchTerms, setSearchTerms] = useState<string>('')
     const { clientes, getClientes, isLoading } = useClientStore();
 
@@ -19,44 +20,55 @@ export default function ListadoClientes() {
       }
 
       const handleChange = (e: string) => {
-        setEstadoFinal(e)   
+        setEstadoFinal(e)  
       }
-
+      const handleDateChange = (e: string) => {
+        setFinalDate(e)  
+      }
+      
       const handleSearchChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerms(e.target.value)
       }
-
-
-
       const clientesFiltrados = clientes.filter(cliente => cliente.nombre.toLowerCase().includes(searchTerms.toLowerCase()))
-
-
 
   return (
 
     <div className="flex flex-col items-center mx-auto p-5 w-full 2xl:w-[1050px]">
-          <div className="bg-white shadow-md rounded-md p-5 w-full 2xl:w-[1050px] mb-5 flex flex-col xl:flex-row items-center justify-start gap-5 xl:gap-20">
-            <h2 className="text-2xl text-slate-600 ml-6">Filtros</h2>
-              <div className="flex gap-3 items-center justify-center">
-                <p className="text-slate-400 font-bold">Estado Actual</p>
-                <select
-                  id="estadoSelectFind"
-                  className=" w-40 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50 p-2"
-                  onChange={(e)=> handleChange(e.target.value)}>
-                    <option value="Todos">Todos</option>
-                    <option value="Pendiente">Pendientes</option>
-                    <option value="Completado">Completados</option>
-                </select>
+          <div className="bg-white shadow-md rounded-md p-5 w-full 2xl:w-[1050px] mb-5 flex flex-row items-center justify-center gap-5 xl:gap-20">
+              <div className="flex flex-col items-start gap-4">
+                <h2 className="text-4xl text-slate-600 font-bold">Filtros</h2>
+                <div className="flex gap-3 items-center justify-center ">
+                  <p className="text-slate-400 font-bold w-20">Nombre</p>
+                  <input 
+                    type="text"
+                    id="buscar"
+                    placeholder="Nombre y Apellido"
+                    className=" w-44 p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50" 
+                    onChange={handleSearchChange}
+                  />
+                </div>
               </div>
-              <div className="flex gap-3 items-center justify-center ">
-                <p className="text-slate-400 font-bold">Nombre</p>
-                <input 
-                  type="text"
-                  id="buscar"
-                  placeholder="Nombre y Apellido"
-                  className=" w-30 p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50" 
-                  onChange={handleSearchChange}
-                />
+              <div className="flex flex-col items-start gap-4">
+                <div className="flex gap-3 items-center justify-center">
+                  <p className="text-slate-400 font-bold w-16">Estado</p>
+                  <select
+                    id="estadoSelectFind"
+                    className=" w-40 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50 p-2"
+                    onChange={(e)=> handleChange(e.target.value)}>
+                      <option value="Todos">Todos</option>
+                      <option value="Pendiente">Pendientes</option>
+                      <option value="Completado">Completados</option>
+                  </select>
+                </div>
+                <div className="flex gap-3 items-center justify-center ">
+                  <p className="text-slate-400 font-bold w-16">Fecha</p>
+                  <input 
+                    type="date"
+                    id="date"
+                    className=" w-40 p-2 bg-transparent border-b-2 focus:outline-none focus:border-b-primary bg-slate-50" 
+                    onChange={(e) => handleDateChange(e.target.value)}
+                  />
+                </div>
               </div>
 
 
@@ -67,7 +79,7 @@ export default function ListadoClientes() {
          <div className={`flex flex-col w-full 2xl:w-[1050px] space-y-3 h-fit ${clientesFiltrados.length ? 'overflow-y-scroll' : ''}`}>
          {clientesFiltrados.length ? (
                 <>
-                  {estadoFinal === 'Todos' ? 
+                  {estadoFinal === 'Todos' && finalDate.toString() === '' ? 
                     clientesFiltrados.map(cliente => 
                       (
                         <ClientDetails 
@@ -78,12 +90,21 @@ export default function ListadoClientes() {
                     )    
                   :                   
                   clientesFiltrados.map(cliente => 
-                    cliente.estado === estadoFinal ? (
+                    cliente.estado === estadoFinal && finalDate === '' ? (
                       <ClientDetails 
                         key={cliente.id}
                         cliente={cliente}
                       />
-                    ) : null
+                    ) : cliente.estado === estadoFinal && cliente.ultimaVisita.toString() === finalDate ? (
+                      <ClientDetails 
+                        key={cliente.id}
+                        cliente={cliente}
+                      />
+                    ) : cliente.ultimaVisita.toString() === finalDate && estadoFinal === 'Todos' ? (
+                      <ClientDetails 
+                        key={cliente.id}
+                        cliente={cliente}
+                      />  ) : null
                   )                  
                   }
                 </>
